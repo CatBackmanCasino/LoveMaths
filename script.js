b58f620b1847827121770046ff7e22c5c07d6d75
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-03-21 13:26:59 +0100

    create empty functions to understand structure

---
+++ script.js
@@ -0,0 +1,9 @@
+function runGame();
+function rightAnswer();
+function calculateCorrectAnswer ();
+function incramentScore();
+function incramentWrongAnswer();
+function displayAdditionQustion();
+function displaySubtractQuestion();
+function displayMultiplyQuestion();
+
