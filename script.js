a312d36bdd34ecc7ba96868e9e61b0d77ac1defe
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-03-21 14:10:04 +0100

    function runGame now generates two random numbers

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-function runGame();
-function rightAnswer();
-function calculateCorrectAnswer ();
-function incramentScore();
-function incramentWrongAnswer();
-function displayAdditionQustion();
-function displaySubtractQuestion();
-function displayMultiplyQuestion();
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("you clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+
+
+        })
+
+        }
+
+    
+})
+  
+
+
+function runGame(){
+
+}
+
+function rightAnswer(){
+
+}
+
+function calculateCorrectAnswer (){
+
+}
+
+function incramentScore(){
+
+}
+
+function incramentWrongAnswer(){
+
+}
+
+function displayAdditionQustion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
 
